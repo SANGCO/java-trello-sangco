eab8ce1ea7039b78c3d6b39ff1f831c6481534a4
sangco <nsg1213@gmail.com>
2018-09-09 23:59:37 +0900

    Initial commit

---
+++ src/main/java/codesquad/TrelloApplication.java
@@ -0,0 +1,13 @@
+package codesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class TrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloApplication.class, args);
+	}
+}
